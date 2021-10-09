1ceb6dbf18b21ec1c83aa65b4834adaf7d9f150b
RahulPraj <rahul971801@gmail.com>
2021-10-10 01:17:45 +0530

    map is add

--- server/index.js
+++ server/index.js
@@ -1,43 +1,48 @@
-// importing Env variables
+// Importing Env Variables
 require("dotenv").config();
 
-//Libraries
+// Libraries
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import passport from "passport";
 
-//configs
+// configs
 import googleAuthConfig from "./config/google.config";
 import routeConfig from "./config/route.config";
-//microservice routes
+
+// microservice routes
 import Auth from "./API/Auth";
 import Restaurant from "./API/Restaurant";
 import Food from "./API/Food";
-import  Image from "./API/Image";
+import Image from "./API/Image";
 import Order from "./API/orders";
 import Reviews from "./API/reviews";
 import User from "./API/User";
+import Menu from "./API/menu";
+import MailService from "./API/Mail";
+import Payments from "./API/Payments";
 
-//Database connection
+// Database connection
 import ConnectDB from "./database/connection";
 
 const zomato = express();
 
-//application middlewares
+console.log(process.env);
+
+// application middlewares
 zomato.use(express.json());
-zomato.use(express.urlencoded({extended:false}));
+zomato.use(express.urlencoded({ extended: false }));
 zomato.use(helmet());
 zomato.use(cors());
 zomato.use(passport.initialize());
 zomato.use(passport.session());
 
-
-//paasport configuration
+// passport cofiguration
 googleAuthConfig(passport);
 routeConfig(passport);
 
-//Application Routes
+// Application Routes
 zomato.use("/auth", Auth);
 zomato.use("/restaurant", Restaurant);
 zomato.use("/food", Food);
@@ -45,10 +50,18 @@ zomato.use("/image", Image);
 zomato.use("/order", Order);
 zomato.use("/reviews", Reviews);
 zomato.use("/user", User);
+zomato.use("/menu", Menu);
+zomato.use("/mail", MailService);
+zomato.use("/payments", Payments);
 
 zomato.get("/", (req, res) => res.json({ message: "Setup success" }));
 
-zomato.listen(4000, () => ConnectDB()
-.then(() => console.log("server is running"))
-.catch (() => console.log("Server is running, but Database connection is failed.."))
+const port = process.env.PORT || 4000;
+
+zomato.listen(port, () =>
+  ConnectDB()
+    .then(() => console.log("Server is running 🚀"))
+    .catch(() =>
+      console.log("Server is running, but database connection failed... ")
+    )
 );
